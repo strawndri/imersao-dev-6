ddc0a8f390ce07ae705837d13e65ab3d2beee955
Andrieli <andrieliluci@gmail.com>
2023-01-30 16:00:55 -0300

    criando desafio 4

---
+++ desafio-4/script.js
@@ -0,0 +1,14 @@
+var nome = 'Andrieli';
+
+var valorAtual = 100;
+var cotacaoDoDolar = 5.13;
+var cotacaoDoPesoArgentino = 0.027;
+var cotacaoDoEuro = 5.57;
+var cotacaoBitcoin = 118342.83;
+
+alert('Olá, ' + nome + '!')
+alert('Vamos saber quanto fica o valor 100.00 de cada moeda em Real?')
+alert("Cotação do Dólar ($) em Real (R$): " + (valorAtual * cotacaoDoDolar).toFixed(2));
+alert("Cotação do Peso Argentino ($) em Real (R$): " + (valorAtual * cotacaoDoPesoArgentino).toFixed(2));
+alert("Cotação do Euro (€) em Real (R$): " + (valorAtual * cotacaoDoEuro).toFixed(2));
+alert("Cotação do Bitcoin (₿) em Real (R$): " + (valorAtual * cotacaoBitcoin).toFixed(2));
