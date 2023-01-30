cae3c9d4aff4465007316dcce79d35cd688d2af4
Andrieli <andrieliluci@gmail.com>
2023-01-30 18:25:08 -0300

    atualizando desafio 1

--- desafio-1/script.js
+++ desafio-1/script.js
@@ -1,12 +1,22 @@
-var nome = 'Andrieli';
+// var nome = 'Andrieli';
 
-var valorAtual = 100;
-var cotacaoDoDolar = 5.13;
-var cotacaoDoPesoArgentino = 0.027;
-var cotacaoDoEuro = 5.57;
+// var cotacaoDoDolar = 5.13;
+// var cotacaoDoPesoArgentino = 0.027;
+// var cotacaoDoEuro = 5.57;
 
-alert('Olá, ' + nome + '!')
-alert('Vamos saber quanto fica o valor 100.00 de cada moeda em Real?')
-alert("Cotação do Dólar ($) em Real (R$): " + (valorAtual * cotacaoDoDolar).toFixed(2));
-alert("Cotação do Peso Argentino ($) em Real (R$): " + (valorAtual * cotacaoDoPesoArgentino).toFixed(2));
-alert("Cotação do Euro (€) em Real (R$): " + (valorAtual * cotacaoDoEuro).toFixed(2));
+// alert('Olá, ' + nome + '!')
+// alert('Vamos saber quanto fica o valor 100.00 de cada moeda em Real?')
+// alert("Cotação do Dólar ($) em Real (R$): " + (valorAtual * cotacaoDoDolar).toFixed(2));
+// alert("Cotação do Peso Argentino ($) em Real (R$): " + (valorAtual * cotacaoDoPesoArgentino).toFixed(2));
+// alert("Cotação do Euro (€) em Real (R$): " + (valorAtual * cotacaoDoEuro).toFixed(2));
+
+
+function converteValor() {
+    var valorAtual = document.getElementById('valor').value
+    var cotacaoDoDolar = 5.13;
+    var cotacaoDoPesoArgentino = 0.027;
+    var cotacaoDoEuro = 5.57;
+    alert("Dólar ($): " + "R$ " + (valorAtual * cotacaoDoDolar).toFixed(2));
+    alert("Peso Argentino ($): " + "R$ " + (valorAtual * cotacaoDoPesoArgentino).toFixed(2));
+    alert("Euro (€): " + "R$ " + (valorAtual * cotacaoDoEuro).toFixed(2));
+}
