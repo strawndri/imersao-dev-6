721111ae57b7d0b72d820a4f304a5530559d4d0b
Andrieli <andrieliluci@gmail.com>
2023-01-30 15:59:00 -0300

    adicionando desafio 3

---
+++ desafio-3/script.js
@@ -0,0 +1,5 @@
+var temperaturaCelsius = 18;
+var temperaturaCelsiusParaFahrenheit = ((18 * 9/5) + 32).toFixed(2);
+var temperaturaCelsiusParaKelvin = temperaturaCelsius + 273
+
+alert(temperaturaCelsius + '°C → ' + temperaturaCelsiusParaFahrenheit + '°F → ' + temperaturaCelsiusParaKelvin + 'K' )
