0fe5b5e62a97291cf8bf0f47935e7286a9f8094c
Andrieli <andrieliluci@gmail.com>
2023-01-30 15:43:01 -0300

    adicionando desafio 2

---
+++ desafio-2/script.js
@@ -0,0 +1,5 @@
+var anosLuz = 9460800000000
+var distanciaEdBAnosLuz = 1480;
+var distanciaEmQuilometros = anosLuz * distanciaEdBAnosLuz;
+
+alert('A EdB é uma estrela rebelde que foge do que já vimos na Via Láctea. Ela encontra-se a ' + distanciaEmQuilometros + 'km da Terra!')
