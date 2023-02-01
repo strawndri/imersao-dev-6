9c70ab9317e19d944ca035fbfead2d94426a66e6
Andrieli <andrieliluci@gmail.com>
2023-02-01 17:39:11 -0300

    adicionando desafios

--- aluraflix/scripts.js
+++ aluraflix/scripts.js
@@ -1,9 +1,73 @@
-var listaFilmes = []
-listaFilmes[0] = 'https://br.web.img3.acsta.net/pictures/210/527/21052756_20131024195513383.jpg'
-listaFilmes[1] = 'https://m.media-amazon.com/images/I/719AMX2PRKL._AC_.gif'
-listaFilmes[2] = 'https://m.media-amazon.com/images/I/71xzYrBb72L._RI_.jpg'
+var books = [
+    {
+        title: "Drácula",
+        cover: "https://m.media-amazon.com/images/I/61MgodE1s0L.jpg"
+    },
+    {
+        title: "Quarto de despejo",
+        cover: "https://m.media-amazon.com/images/I/81ykDTJWx9L.jpg"
+    },
+    {
+        title: "Diário de Anne Frank",
+        cover: "https://m.media-amazon.com/images/I/91fYMzrTIyL.jpg"
+    },
+    {
+        title: "O Guia do mochileiro das galáxias",
+        cover: "https://m.media-amazon.com/images/I/91rGyWfZN8L.jpg"
+    }
+];
 
-for (var i = 0; i < listaFilmes.length; i++) {
-    document.write('<img src="' + listaFilmes[i] + '">')
-    
+booksElement = document.querySelector(".books");
+var bookExists = false;
+var imageExtensions = ["jpg", "png", "jpeg", "gif", "svg", "tiff"];
+
+function showBooks() {
+    booksElement.innerHTML = "";
+    books.forEach((book) => {
+        var newDiv = document.createElement("div");
+        var newImage = document.createElement("img");
+        var newTitle = document.createElement("h2");
+        newImage.src = book.cover;
+        newTitle.innerText = book.title;
+        newDiv.appendChild(newImage);
+        newDiv.appendChild(newTitle);
+        booksElement.appendChild(newDiv);
+    });
 }
+
+function addNewBook() {
+    var bookTitle = document.getElementById("bookTitle").value;
+    var bookCover = document.getElementById("bookCover").value;
+    
+    var url = bookCover.split(".");
+    var extension = url[url.length - 1];
+    
+    if (bookTitle == "" || bookCover == "") {
+        alert("Não foi possível adicionar o livro. Tente novamente.");
+    } else if (imageExtensions.includes(extension) == false) {
+        alert(
+            "Oops! Não foi possível adicionar a imagem. Tente um caminho com alguma destas extensões: jpg, png, jpeg, gif, svg ou tiff."
+            );
+        } else {
+            
+            for (var i = 0; i < books.length; i++) {
+                if (
+                    bookTitle.toLowerCase() == books[i].title.toLowerCase() ||
+                    bookCover == books[i].cover
+                    ) {
+                        alert(bookTitle + " já está na lista de livros. Tente adicionar outro.");
+                        bookExists = true;
+                        break
+                    } 
+                }  
+                
+                if (bookExists == false) {
+                    books.push({
+                        title: bookTitle,
+                        cover: bookCover
+                    });
+                    showBooks();
+                }
+            }
+        }
+        showBooks();
---
+++ desafios/scripts.js
@@ -0,0 +1,75 @@
+var books = [
+    {
+      title: "Drácula",
+      cover: "https://m.media-amazon.com/images/I/61MgodE1s0L.jpg"
+    },
+    {
+      title: "Quarto de despejo",
+      cover: "https://m.media-amazon.com/images/I/81ykDTJWx9L.jpg"
+    },
+    {
+      title: "Diário de Anne Frank",
+      cover: "https://m.media-amazon.com/images/I/91fYMzrTIyL.jpg"
+    },
+    {
+      title: "O Guia do mochileiro das galáxias",
+      cover: "https://m.media-amazon.com/images/I/91rGyWfZN8L.jpg"
+    }
+  ];
+  
+  booksElement = document.querySelector(".books");
+  var bookExists = false;
+  var imageExtensions = ["jpg", "png", "jpeg", "gif", "svg", "tiff"];
+  
+  function showBooks() {
+    booksElement.innerHTML = "";
+    books.forEach((book) => {
+      var newDiv = document.createElement("div");
+      var newImage = document.createElement("img");
+      var newTitle = document.createElement("h2");
+      newImage.src = book.cover;
+      newTitle.innerText = book.title;
+      newDiv.appendChild(newImage);
+      newDiv.appendChild(newTitle);
+      booksElement.appendChild(newDiv);
+    });
+  }
+  
+  function addNewBook() {
+    var bookTitle = document.getElementById("bookTitle").value;
+    var bookCover = document.getElementById("bookCover").value;
+  
+    var url = bookCover.split(".");
+    var extension = url[url.length - 1];
+  
+    if (bookTitle == "" || bookCover == "") {
+      alert("Não foi possível adicionar o livro. Tente novamente.");
+    } else if (imageExtensions.includes(extension) == false) {
+      alert(
+        "Oops! Não foi possível adicionar a imagem. Tente um caminho com alguma destas extensões: jpg, png, jpeg, gif, svg ou tiff."
+      );
+    } else {
+      for (var i = 0; i < books.length; i++) {
+        if (
+          bookTitle.toLowerCase() == books[i].title.toLowerCase() ||
+          bookCover == books[i].cover
+        ) {
+          alert(
+            bookTitle + " já está na lista de livros. Tente adicionar outro."
+          );
+          bookExists = true;
+          break;
+        }
+      }
+  
+      if (bookExists == false) {
+        books.push({
+          title: bookTitle,
+          cover: bookCover
+        });
+        showBooks();
+      }
+    }
+  }
+  showBooks();
+  
