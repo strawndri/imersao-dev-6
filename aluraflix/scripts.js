764a975557559b3af9fc517466bc1e7473d85681
Andrieli <andrieliluci@gmail.com>
2023-02-02 17:12:59 -0300

    adicionando projeto desenvolvido em aula

---
+++ aluraflix/scripts.js
@@ -0,0 +1,8 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById('filme').value
+    var elementoListaFilmes = document.getElementById('listaFilmes')
+    
+    elementoListaFilmes.innerHTML += '<img src="' + filmeFavorito + '">'
+    
+    document.getElementById('filme').value = ''
+}
