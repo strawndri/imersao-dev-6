dec6e7a8a3ced843686fb8f3bc2ac163ce5fe1e0
Andrieli <andrieliluci@gmail.com>
2023-02-02 17:40:01 -0300

    atualizando desafio 3 com novas funcionalidades e design

--- desafio-3/script.js
+++ desafio-3/script.js
@@ -1,5 +1,69 @@
-var temperaturaCelsius = 18;
-var temperaturaCelsiusParaFahrenheit = ((18 * 9/5) + 32).toFixed(2);
-var temperaturaCelsiusParaKelvin = temperaturaCelsius + 273
-
-alert(temperaturaCelsius + '°C → ' + temperaturaCelsiusParaFahrenheit + '°F → ' + temperaturaCelsiusParaKelvin + 'K' )
+function convert() {
+    var fromElement = document.getElementById("from");
+    var toElement = document.getElementById("to");
+    var from = fromElement.options[fromElement.selectedIndex].value;
+    var to = toElement.options[toElement.selectedIndex].value;
+  
+    var valueElement = parseInt(document.getElementById("value").value);
+  
+    if (valueElement == "" && valueElement != 0) {
+      alert(
+        "Não foi possível realizar a conversão. Insira um valor no campo de texto."
+      );
+    } else if (from == to) {
+      alert(
+        "Não foi possível realiar a conversão. É necessário escolher duas medidas distintas."
+      );
+    } else {
+      showResult(from, to, valueElement);
+    }
+  }
+  
+  function showResult(from, to, value) {
+    let resultElement = document.querySelector(".result");
+    resultElement.innerHTML = ""
+    let paragraphElement = document.createElement("p");
+    let result;
+    let fromSymbol;
+    let toSymbol;
+    let symbols = ["°C", "°F", "K"];
+  
+    switch (from) {
+      case "celsius":
+        fromSymbol = symbols[0];
+  
+        if (to == "fahrenheit") {
+          toSymbol = symbols[1];
+          result = ((value * 9) / 5 + 32).toFixed(2);
+        } else {
+          toSymbol = symbols[2];
+          result = value + 273;
+        }
+        break;
+      case "fahrenheit":
+        fromSymbol = symbols[1];
+        if (to == "celsius") {
+          toSymbol = symbols[0];
+          result = (((value - 32) * 5) / 9).toFixed(2);
+        } else {
+          toSymbol = symbols[2];
+          result = (((value - 32) * 5) / 9).toFixed(2) + 273;
+        }
+        break;
+      case "kelvin":
+        fromSymbol = symbols[2];
+        if (to == "celsius") {
+          toSymbol = symbols[0];
+          result = value - 273;
+        } else {
+          toSymbol = symbols[1];
+          result = (((value - 273) * 9) / 5 + 32).toFixed(2);
+        }
+        break;
+    }
+  
+    paragraphElement.innerText =
+      value + fromSymbol + " equivale a " + result + toSymbol;
+    resultElement.appendChild(paragraphElement);
+  }
+  
